9570d5818de0739411717abc0e9dc918991b4af2
AntoineDoyon <50877055+AntoineDoyon@users.noreply.github.com>
2019-11-29 13:55:38 -0500

    Update image_recon.cs

--- image_recon.cs
+++ image_recon.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 // Specify a Google Cloud Storage uri for the image
 // or a publicly accessible HTTP or HTTPS uri.
-var image = Image.FromUri(uri);
+var image = Image.FromUri('https://github.com/AntoineDoyon/AI_Test/10390-Flexcom-4729-2019-03-27.pdf');
 var client = ImageAnnotatorClient.Create();
 var response = client.DetectDocumentText(image);
 foreach (var page in response.Pages)
